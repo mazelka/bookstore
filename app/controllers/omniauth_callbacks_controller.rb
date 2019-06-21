cc165f09e5de9f9d8f650d26d7a211ea9e2ff475
Mary Banshee <mary.banshee@gmail.com>
2019-06-21 11:42:55 +0300

    wip: customer new password

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,9 +1,10 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     customer = Customer.from_omniauth(request.env['omniauth.auth'])
+    binding.pry
     if customer.persisted?
-      sign_in_and_redirect customer
       flash.notice = 'Successfully signed in!'
+      sign_in_and_redirect customer
     else
       session['devise.customer_attributes'] = customer.attributes
       redirect_to new_customer_registration_path
