2484f00d3f14c8771addc72255cee773dfee9a36
Mary Banshee <mary.banshee@gmail.com>
2019-07-17 15:23:00 +0300

    change port in mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'mazelka-bookstore.herokuapp.com', port: 3000 }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => 'mazelka-bookstore.herokuapp.com', :port => 1025 }
+  config.action_mailer.smtp_settings = { :address => 'mazelka-bookstore.herokuapp.com', :port => 2525 }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
