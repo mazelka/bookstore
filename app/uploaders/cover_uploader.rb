3a7abb91f397ae20ee37300ab223afad56b20b5c
Mary Banshee <mary.banshee@gmail.com>
2019-07-18 12:15:56 +0300

    add version of book cover for home and show pages

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -34,6 +34,10 @@ class CoverUploader < CarrierWave::Uploader::Base
     process resize_to_limit: [150, 150]
   end
 
+  version :full do
+    process :resize_to_limit => [400, 650]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_whitelist
